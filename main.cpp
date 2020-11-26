34e21cc381c33f6d30ef90180554d41861588b68
diogomarchi <diogo.marchi@hotmail.com>
2020-11-26 11:31:39 -0300

    Gera trajeto e avalia trajeto implementado

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <time.h>
 
 using namespace std;
 
@@ -35,39 +36,15 @@ Problemas lerProblemas(const std::string& caminho);
 /* retorna a distancia entre duas cidades */
 int retornaDistacia (Problema problema, int cidade1, int cidade2);
 
+/* Calcula distancia do trajeto */
+void avaliar (Solucao& s, Problema problema);
 
-int avaliar (Solucao& s){
-    int distancia_total = 0;
+/* Gera trajeto randomicamente */
+void geraTrajeto (Solucao& s, int num_cidades);
 
-    // TODO calcular distancia total
+/* Gera populacoa inicial */
+std::vector<Solucao> populacaoInicial(Problema problema, int tamanho_populacao);
 
-    return distancia_total;
-}
-
-std::vector<int> geraTrajeto (int num_cidades){
-    std::vector<int> trajeto;
-
-    // TODO gerar trajeto
-
-    return trajeto;
-}
-
-/* retorna uma população inicial */
-std::vector<Solucao> populacaoInicial(Problema problema, int tamanho_populacao){
-    std::vector<Solucao> populacao;
-
-    int num_cidades = problema[0].size();
-    int cidade_aleatoria = 0;
-
-    for(int i = 0; i < tamanho_populacao; i++){
-        Solucao solucao;
-        solucao.trajeto = geraTrajeto(num_cidades);
-        avaliar(solucao);
-        populacao.push_back(solucao);
-    }
-
-    return populacao;
-}
 
 /**
     main
@@ -75,10 +52,28 @@ std::vector<Solucao> populacaoInicial(Problema problema, int tamanho_populacao){
 
 int main()
 {
+    srand(time(NULL));
     Problemas problemas;
-    problemas = lerProblemas("C:/Users/User/Desktop/univali/IA/AG_caixeiro_viajante/problemas.csv");
+    problemas = lerProblemas("/home/diogomarchi/Documentos/FACULDADE-MATERIAS/inteligenciaArtificial/AG_caixeiro_viajante/problemas.csv");
+    //problemas = lerProblemas("C:/Users/User/Desktop/univali/IA/AG_caixeiro_viajante/problemas.csv");
 
-    cout << "distancia " << retornaDistacia(problemas[0], 2, 1) << endl;
+    Solucao solucao;
+    solucao.trajeto = geraTrajeto(5);
+    solucao.distancia = avaliar(solucao, problemas[0]);
+    std::vector<Solucao> populacao;
+    populacao = populacaoInicial(problemas[0], 5);
+
+    /* Mostra populacao inicial gerada */
+    for(int j = 0; j < 5; j ++){
+
+        for(int i = 0; i < 5; i ++){
+            cout << "[" << i << "] = " << populacao[j].trajeto[i] + 1 << "  ";
+        }
+
+        cout << "\n distancia solucao[" << j << "] = " << populacao[j].distancia << endl;
+        cout << endl << endl;
+    }
+
 
     return 0;
 }
@@ -88,7 +83,6 @@ int main()
 Problema processarLinha(const std::string& linha) {
     Problema resultado;
     std::vector<int> distancias;
-
     std::string pedaco;
     std::istringstream stream(linha);
 
@@ -143,6 +137,58 @@ Problemas lerProblemas(const std::string& caminho) {
 
 /* retorna a distancia entre duas cidades */
 int retornaDistacia (Problema problema, int cidade1, int cidade2){
+
+/* Calcula distancia do trajeto */
+void avaliar (Solucao& s, Problema problema){
+
+/* Gera trajeto randomicamente */
+void geraTrajeto (Solucao& s, int num_cidades){
+    int aux = 0, valorCorreto;
+
+    // gera trajeto
+    for(int i = 0; i < num_cidades; i++){
+        do{
+            aux = rand()%num_cidades;                // gera um valor randomico
+            valorCorreto = 1;
+
+            for(int j = 0; j < trajeto.size(); j ++){//verifica se auxiliar é igual a algum valor do trajeto
+                if(aux == trajeto.at(j))
+                    valorCorreto = 0;
+            }
+
+        }while(valorCorreto == 0);
+
+        s.trajeto.push_back(aux);                      // Coloca a cidade no trajeto
+    }
+}
+
+/* Gera populacao inicial */
+std::vector<Solucao> populacaoInicial(Problema problema, int tamanho_populacao){
+    std::vector<Solucao> populacao;
+
+    int num_cidades = problema[0].size();
+    int cidade_aleatoria = 0;
+
+    // Gera populacao inicial chamando as funcoes: gerar trajeto e avaliar
+    for(int i = 0; i < tamanho_populacao; i++){
+        Solucao solucao;
+        geraTrajeto(solucao, num_cidades);
+        avaliar(solucao, problema);
+        populacao.push_back(solucao);
+    }
+
+    return populacao;
+}
+    int distancia_total = 0;
+
+    // calcula distancia total do trajeto gerado
+    for(int i = 0; i < s.trajeto.size() - 1; i ++){
+        distancia_total += retornaDistacia(problema, s.trajeto.at(i), s.trajeto.at(i+1));
+    }
+
+    // atribui a distancia no trajeto da solucao
+    s.distancia = distancia_total;
+}
     int distancia = 0;
 
     /* verifica se o valor da cidade está dentro dos limites */
